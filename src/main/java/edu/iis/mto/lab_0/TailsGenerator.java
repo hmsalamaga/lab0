e471cc1ca2ecb4774b894e24eb50c1e645579dd0
hubert-salamaga <hmsalamaga@gmail.com>
2020-03-03 18:19:58 +0100

    Finish TailsGenerator

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        int array_length = value.length() + 1;
+        ArrayList<String> tails = new ArrayList<>(array_length);
+
+        for (int i = 0; i < array_length; i++) {
+            tails.add(i, value.substring(i));
+        }
+
+        return tails;
     }
 
 }
